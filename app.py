f32e2bfc6b783d16e0f68e69cb800819b904d3e2
smitpatel004 <thesmitpatel1@gmail.com>
2024-11-01 09:23:26 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask,request,jsonify
+import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+app = Flask(__name__)
+
+@app.route('/',methods=['POST'])
+def index():
+    data = request.get_json()
+    source_currency = data['queryResult']['parameters']['unit-currency']['currency']
+    amount = data['queryResult']['parameters']['unit-currency']['amount']
+    target_currency = data['queryResult']['parameters']['currency-name']
+
+
+    cf = fetch_conversion_factor(source_currency,target_currency)
+    final_amount = amount * cf
+    final_amount = round(final_amount,2)
+    response = {
+        'fulfillmentText':"{} {} is {} {}".format(amount,source_currency,final_amount,target_currency)
+    }
+    return jsonify(response)
+
+def fetch_conversion_factor(source,target):
+    api_key=os.getenv('API_KEY')
+    url="https://free.currconv.com/api/v7/convert?q={}_{}&compact=ultra&apiKey={}".format(source,target, api_key)
+
+    response = requests.get(url)
+    response = response.json()
+
+    return response['{}_{}'.format(source,target)]
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
